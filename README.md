19c248255f74dae9ff9b0eaf7413fd4a56b7ce07
Michael Schuetze <mjschuetze102@gmail.com>
2022-03-13 15:13:40 -0400

    Created example slack app implementation

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # TriggerRepro
 Repro for issue with chat.update using Slack Bolt API
+
+## Summary of Issue
+
+The original message that I'm attempting to update comes from the `ack` response to a command:
+
+```java
+private static void startCommand(App app) {
+    app.command("/startChatUpdateRepro", (req, ctx) ->
+        ctx.ack(res -> res
+                .responseType("in_channel")
+                .blocks(getBlocks(null))
+        )
+    );
+}
+```
+
+Within the blocks of the message is an action button that when clicked should cause the message to update:
+    
+```java
+private static void joinAction(App app) {
+    app.blockAction("join_action", (req, ctx) -> {
+
+        // The following does not work, resulting in a cant_update_message error
+        ChatUpdateResponse chatUpdateRes = ctx.client().chatUpdate(r -> r
+                .blocks(getBlocks(ctx.getRequestUserId()))
+                .channel(req.getPayload().getChannel().getId())
+                .ts(req.getPayload().getMessage().getTs())
+                .token(ctx.getBotToken())
+        );
+
+        if (chatUpdateRes.isOk()) return ctx.ack();
+        else return Response.builder().statusCode(500).body(chatUpdateRes.getError()).build();
+    });
+}
+```
+
+However, the above code does not work, always resulting in a `cant_update_message` error. The following
+does work, but the documentation gives the impression that the above should work as well.
+
+```java
+private static void joinAction(App app) {
+    app.blockAction("join_action", (req, ctx) -> {
+
+        ctx.respond(res -> res
+                .blocks(getBlocks(ctx.getRequestUserId()))
+                .replaceOriginal(true)
+        );
+
+        return ctx.ack();
+    });
+}
+```
+
---
+++ src/main/java/ChatUpdate.java
@@ -0,0 +1,79 @@
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.bolt.response.Response;
+import com.slack.api.methods.response.chat.ChatUpdateResponse;
+import com.slack.api.model.block.LayoutBlock;
+
+import java.util.List;
+
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.*;
+
+public class ChatUpdate {
+    private static void startCommand(App app) {
+        app.command("/startChatUpdateRepro", (req, ctx) ->
+            ctx.ack(res -> res
+                    .responseType("in_channel")
+                    .blocks(getBlocks(null))
+            )
+        );
+    }
+
+    private static void joinAction(App app) {
+        app.blockAction("join_action", (req, ctx) -> {
+
+            // The following does not work, resulting in a cant_update_message error
+            ChatUpdateResponse chatUpdateRes = ctx.client().chatUpdate(r -> r
+                    .blocks(getBlocks(ctx.getRequestUserId()))
+                    .channel(req.getPayload().getChannel().getId())
+                    .ts(req.getPayload().getMessage().getTs())
+                    .token(ctx.getBotToken())
+            );
+
+            if (chatUpdateRes.isOk()) return ctx.ack();
+            else return Response.builder().statusCode(500).body(chatUpdateRes.getError()).build();
+
+//            // The following does work, but documentation leads to believe the above should work as well
+//            ctx.respond(res -> res
+//                    .blocks(getBlocks(ctx.getRequestUserId()))
+//                    .replaceOriginal(true)
+//            );
+//            return ctx.ack();
+        });
+    }
+
+    private static List<LayoutBlock> getBlocks(String userId) {
+        return asBlocks(
+                context(context -> context
+                        .elements(asContextElements(
+                                markdownText(mt -> mt
+                                    .text(String.format(
+                                          "Hello, %s", userId == null ? "be the first to join" : userId
+                                    ))
+                                )
+                        ))
+                ),
+                divider(),
+                actions(action -> action
+                        .elements(asElements(
+                                button(b -> b.actionId("join_action")
+                                        .text(plainText(pt -> pt.text("Join")))
+                                        .value("temp")
+                                )
+                        ))
+                )
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+
+        startCommand(app);
+        joinAction(app);
+
+        SlackAppServer server = new SlackAppServer(app);
+        server.start();
+    }
+}
